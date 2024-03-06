475edaca59634eabbc3bd6aa442d9225b1de6b4f
Vinicius R <vrodrigues.code@gmail.com>
2024-03-05 22:41:16 -0300

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
 # Update and upgrade system
-sudo pacman -Syu --noconfirm
+sudo pacman -Syu
 
 # Terminal utilities
-sudo pacman -S neofetch alacritty kitty micro ranger --needed --noconfirm
-
-# App launcher
-sudo pacman -S wofi --needed --noconfirm
+sudo pacman -S neofetch alacritty micro ranger htop
 
 # Install Xorg, Bspwm and essentials
-sudo pacman -S xorg-server xorg-xinit picom bspwm sxhkd nitrogen wofi --needed --noconfirm
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot picom bspwm sxhkd nitrogen rofi lxappearance-gtk3 gnome-themes-extra
 
 # File manager
-sudo pacman -S thunar thunar-archive-plugin thunar-volman --needed --noconfirm
+sudo pacman -S thunar thunar-archive-plugin thunar-volman
 
 # Sound Packages
-sudo pacman -S pamixer playerctl --needed --noconfirm
+sudo pacman -S pamixer playerctl
+
+# Nvidia Proprietary Drivers (Works for Turing)
+sudo pacman -S nvidia nvidia-utils nvidia-settings nvidia-open
+
+# Bluetooth 
+sudo pacman -S bluez bluez-utils blueman
+sudo systemctl enable bluetooth
 
 # Fonts and icons
-sudo pacman -S ttf-font-awesome noto-fonts ttf-roboto ttf-firacode-nerd --needed --noconfirm
+sudo pacman -S ttf-font-awesome noto-fonts ttf-roboto ttf-firacode-nerd
 
 # Protocols
-sudo pacman -S libnotify --needed --noconfirm
+sudo pacman -S avahi gvfs dosfstools libnotify
+sudo systemctl enable avahi-daemon
 
 # Others
-sudo pacman -S flameshot chromium vscodium --needed --noconfirm
+sudo pacman -S flameshot chromium firefox
+
+# Btrfs only
+sudo pacman -S snapper
